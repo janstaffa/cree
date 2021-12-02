ce332a6b5f8be6b72d2e0e58db2668ba3d014205
Jan Šfaffa <agenttupesy@gmail.com>
2021-12-02 22:15:36 +0100

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use futures::Future;
+use hyper::{service::Service, Body, Method, Request, Response, Result as HyperResult, StatusCode};
+use std::io::Write;
+use std::{
+    fs::{self},
+    path::PathBuf,
+    pin::Pin,
+    process::Command,
+    task::{Context, Poll},
+};
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
+
+static NOTFOUND: &[u8] = b"Not Found";
 /// returns tuple (FILE_NAME, EXTENSION)
 pub fn get_file_meta(file_name: &str) -> (Option<String>, Option<String>) {
     let split = file_name.split('.');
@@ -9,4 +23,124 @@ pub fn get_file_meta(file_name: &str) -> (Option<String>, Option<String>) {
     let file_name = name_vec[..len - 1].join("");
     let extension = name_vec[len - 1];
     (Some(file_name), Some(extension.to_owned()))
+}
+
+pub struct CreeService {
+    root_dir: PathBuf,
+}
+
+impl CreeService {
+    pub fn new(root_dir: PathBuf) -> CreeService {
+        CreeService { root_dir }
+    }
+}
+
+impl Service<Request<Body>> for CreeService {
+    type Response = Response<Body>;
+    type Error = hyper::Error;
+
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        if req.method() == Method::GET {
+            let raw_request_path = req.uri().path().to_owned();
+
+            let request_path = PathBuf::from(&raw_request_path);
+
+            let concatinated = format!("{}{}", self.root_dir.display(), request_path.display());
+            let final_path = PathBuf::from(&concatinated);
+
+            let abs_root_path = self.root_dir.canonicalize().unwrap();
+
+            if !final_path.exists()
+                || !final_path
+                    .canonicalize()
+                    .unwrap()
+                    .starts_with(abs_root_path)
+            {
+                return Box::pin(async { Ok(not_found()) });
+            }
+
+            if final_path.is_dir() {
+                let dir_files = fs::read_dir(&final_path).unwrap();
+                for file in dir_files {
+                    let file = file.unwrap();
+
+                    if file.file_name() == "index.html" {
+                        return Box::pin(hadle_file(file.path()));
+                    }
+                }
+            } else if final_path.is_file() {
+                return Box::pin(hadle_file(final_path));
+            } else {
+                return Box::pin(async { Ok(not_found()) });
+            }
+        }
+        Box::pin(async { Ok(not_found()) })
+    }
+}
+pub struct CreeServer {
+    root_dir: PathBuf,
+}
+
+impl CreeServer {
+    pub fn new(root_dir: PathBuf) -> CreeServer {
+        CreeServer { root_dir }
+    }
+}
+impl<T> Service<T> for CreeServer {
+    type Response = CreeService;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _: T) -> Self::Future {
+        let root_dir = self.root_dir.clone();
+        let cree_service = CreeService::new(root_dir);
+        let fut = async move { Ok(cree_service) };
+        Box::pin(fut)
+    }
+}
+
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(NOTFOUND.into())
+        .unwrap()
+}
+
+async fn hadle_file(path: PathBuf) -> HyperResult<Response<Body>> {
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+    let file_meta = get_file_meta(file_name);
+    if let (Some(filename), Some(extension)) = file_meta {
+        let mut final_path = path;
+        if extension == "php" {
+            let php_result = Command::new("./src/php/x64_win/php-cgi.exe")
+                .arg(&final_path)
+                .output()
+                .expect("ls command failed to start");
+            let tmp_dir = std::env::temp_dir();
+            let tmp_path = tmp_dir.to_path_buf().join(filename + ".html");
+            let mut tmp_file = fs::File::create(&tmp_path).unwrap();
+            tmp_file.write(&php_result.stdout).unwrap();
+            final_path = tmp_path;
+        }
+        return send_file(final_path).await;
+    }
+    return Ok(not_found());
+}
+async fn send_file(path: PathBuf) -> HyperResult<Response<Body>> {
+    if let Ok(file) = File::open(&path).await {
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let body = Body::wrap_stream(stream);
+        return Ok(Response::new(body));
+    }
+    return Ok(not_found());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
+use std::net::SocketAddr;
+use std::path::PathBuf;
+
 use clap::App;
 use clap::Arg;
 use clap::SubCommand;
-use cree::get_file_meta;
-use std::fs;
-use std::io::Write;
-use std::net::SocketAddr;
-use std::path::Path;
-use std::path::PathBuf;
-use std::process::Command;
-use tokio::fs::File;
 
-use tokio_util::codec::{BytesCodec, FramedRead};
-
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Result, Server, StatusCode};
-
-static NOTFOUND: &[u8] = b"Not Found";
+use cree::CreeServer;
+use hyper::Server;
 
 #[tokio::main]
 async fn main() {
@@ -48,16 +39,11 @@ async fn main() {
             return eprintln!("server error: Path {} is not a valid path", &path);
         }
 
-        let addr = SocketAddr::from(([127, 0, 0, 1], port));
+        let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
-        let make_svc = make_service_fn(move |_conn| {
-            let _path = _path.clone();
-            async move {
-                Ok::<_, hyper::Error>(service_fn(move |_req| file_server(_req, _path.clone())))
-            }
-        });
+        let cree_server = CreeServer::new(PathBuf::from(path));
 
-        let server = Server::bind(&addr).serve(make_svc);
+        let server = Server::bind(&addr).serve(cree_server);
 
         println!("Listening on http://{}", addr);
 
@@ -65,78 +51,6 @@ async fn main() {
             eprintln!("server error: {}", e);
         }
     }
-}
-
-async fn file_server(req: Request<Body>, path: PathBuf) -> Result<Response<Body>> {
-    if req.method() == Method::GET {
-        let raw_request_path = req.uri().path().to_owned();
-
-        let request_path = PathBuf::from(&raw_request_path);
-
-        let concatinated = format!("{}{}", path.display(), request_path.display());
-        let mut final_path = PathBuf::from(&concatinated);
-
-        let abs_root_path = path.canonicalize().unwrap();
-
-        if !final_path.exists()
-            || !final_path
-                .canonicalize()
-                .unwrap()
-                .starts_with(abs_root_path)
-        {
-            return Ok(not_found());
-        }
-
-        if final_path.is_dir() {
-            let dir_files = fs::read_dir(&final_path).unwrap();
-            for file in dir_files {
-                let file = file.unwrap();
-
-                if file.file_name() == "index.html" {
-                    return hadle_file(file.path()).await;
-                }
-            }
-        } else if final_path.is_file() {
-            return hadle_file(final_path).await;
-        } else {
-            return Ok(not_found());
-        }
-    }
-    Ok(not_found())
-}
-
-async fn hadle_file(path: PathBuf) -> Result<Response<Body>> {
-    let file_name = path.file_name().unwrap().to_str().unwrap();
-    let file_meta = get_file_meta(file_name);
-    if let (Some(filename), Some(extension)) = file_meta {
-        let final_path = &path;
-        if extension == "php" {
-            let php_result = Command::new("./src/php/x64_win/php.exe")
-                .arg(&final_path)
-                .output()
-                .expect("ls command failed to start");
-            let mut tmp_dir = std::env::temp_dir();
-            let tmp_path = tmp_dir.to_path_buf().join(filename + ".html");
-            let mut tmp_file = fs::File::create(&tmp_path).unwrap();
-            tmp_file.write(&php_result.stdout).unwrap();
-            return send_file(tmp_path).await;
-        }
-    }
-    return Ok(not_found());
-}
-async fn send_file(path: PathBuf) -> Result<Response<Body>> {
-    if let Ok(file) = File::open(&path).await {
-        let stream = FramedRead::new(file, BytesCodec::new());
-        let body = Body::wrap_stream(stream);
-        return Ok(Response::new(body));
-    }
-    return Ok(not_found());
-}
-fn not_found() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(NOTFOUND.into())
-        .unwrap()
 }
 
 // TODO: add php support
